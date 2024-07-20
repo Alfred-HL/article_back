69e69ca68551c7203ee57c18def8a1341c9d8da7
Alfred-HL <jeff434345@hotmail.com>
2024-07-20 10:43:56 +0800

    add editMessage function in models to do all writing

--- models/message.js
+++ models/message.js
@@ -24,6 +24,19 @@ function readMessage(callback) {
   });
 }
 
+//write to message.json file
+function editMessage(messages, callback) {
+  fs.writeFile('./public/messages.json', JSON.stringify({ posts: messages }, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
+}
+
+//add message
 function writeMessage(newMessage, callback) {
   readMessage((err, messages) => {
     if (err) {
@@ -36,17 +49,18 @@ function writeMessage(newMessage, callback) {
 
     messages.push(newMessage);
 
-    fs.writeFile('./public/messages.json', JSON.stringify({ posts: messages }, null, 2), (err) => {
+    editMessage(messages, (err) => {
       if (err) {
-        console.error('Error writing file:', err);
         callback(err);
       } else {
         callback(null, newMessage);
       }
     });
+
   })
 }
 
+//modify message
 function updateMsg(updatedMessage, callback) {
   const postId = Number(updatedMessage.messageId);
   const updatedContent = updatedMessage.content;
@@ -66,9 +80,8 @@ function updateMsg(updatedMessage, callback) {
 
     messages[index].content = updatedContent
 
-    fs.writeFile('./public/messages.json', JSON.stringify({ posts: messages }, null, 2), (err) => {
+    editMessage(messages, (err) => {
       if (err) {
-        console.error('Error writing file:', err);
         callback(err);
       } else {
         callback(null, updatedMessage);
@@ -78,6 +91,7 @@ function updateMsg(updatedMessage, callback) {
   )
 }
 
+//delete message
 function deleteMsg(postId, callback) {
   readMessage((err, messages) => {
     if (err) {
@@ -86,17 +100,10 @@ function deleteMsg(postId, callback) {
     }
     const index = messages.findIndex(p => p.messageId === postId);
 
-    // if (index === -1) {
-    //   // res.status(404).send('Post not found');
-    //   // return;
-    //   console.error('Error searching file:', err);
-    //   callback(err);
-    // }
     const deletedPost = messages.splice(index, 1)[0];
 
-    fs.writeFile('./public/messages.json', JSON.stringify({ posts: messages }, null, 2), (err) => {
+    editMessage(messages, (err) => {
       if (err) {
-        console.error('Error deleting file:', err);
         callback(err);
       } else {
         callback(null, deletedPost);
@@ -105,11 +112,6 @@ function deleteMsg(postId, callback) {
 
   })
 }
-
-
-
-
-
 
 
 module.exports = { 
